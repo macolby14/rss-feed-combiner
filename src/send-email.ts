7fc03479600d1809fabc23ced482dc9463913fb0
Mark Colby <macolby14@gmail.com>
2021-06-06 09:07:46 -1000

    fix assert installation

--- src/send-email.ts
+++ src/send-email.ts
@@ -1,14 +1,15 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 import sgMail from "@sendgrid/mail";
-import assert from "assert";
+import { strict as assert } from "assert";
 
 export async function sendTestEmail(): Promise<void> {
   assert.ok(
     typeof process.env.SENDGRID_API_KEY === "string",
     "SENDGRID_API_KEY is not a string"
   );
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  console.log(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
   const msg = {
     to: "macolby14@gmail.com", // Change to your recipient
     from: "macolby14@gmail.com", // Change to your verified sender
@@ -16,12 +17,12 @@ export async function sendTestEmail(): Promise<void> {
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
   };
-  await sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  // await sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log("Email sent");
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 }
