176d60d272469f2ec70e5fb223f939e7f8a42e56
Mark Colby <macolby14@gmail.com>
2021-06-26 10:46:11 -1000

    read old feeds for comparison

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import "./loadEnv";
-import { getFeeds } from "./getFeeds";
+import { CombinedFeeds, getFeeds } from "./getFeeds";
 import { sendEmail } from "./sendEmail";
 import { writeFile } from "./utilities/writeFile";
+import { promises as fs } from "fs";
 
 (async function main() {
-  const combinedFeeds = await getFeeds();
-
-  writeFile("./out/outputFeed.json", combinedFeeds);
-
-  sendEmail(combinedFeeds);
+  // const combinedFeeds = await getFeeds();
+  // writeFile("./out/outputFeed.json", combinedFeeds);
+  // sendEmail(combinedFeeds);
+  readPreviousFeedsFromFile();
 })();
+
+async function readPreviousFeedsFromFile(): Promise<CombinedFeeds> {
+  const oldFeedString = await fs.readFile("./out/outputFeed.json", "utf8");
+  const oldFeed = JSON.parse(oldFeedString);
+  return oldFeed as CombinedFeeds;
+}
+
+// function compareFeeds(
+//   oldFeed: CombinedFeeds,
+//   newFeed: CombinedFeeds
+// ): CombinedFeeds {
+
+// }
