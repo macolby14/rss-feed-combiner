6c2ada8b296be592d109d9d1968efec1da52c228
Mark Colby <macolby14@gmail.com>
2021-06-26 15:23:14 -1000

    add - method to compare new and previous feeds

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import "./loadEnv";
-import { CombinedFeeds, getFeeds } from "./getFeeds";
+import { CombinedFeeds, getFeeds, ProcessedFeed } from "./getFeeds";
 import { sendEmail } from "./sendEmail";
 import { writeFile } from "./utilities/writeFile";
 import { promises as fs } from "fs";
+import { strict as assert } from "assert";
 
 (async function main() {
-  // const combinedFeeds = await getFeeds();
+  const previousFeeds = await readPreviousFeedsFromFile();
+  const combinedFeeds = await getFeeds();
   // writeFile("./out/outputFeed.json", combinedFeeds);
   // sendEmail(combinedFeeds);
-  readPreviousFeedsFromFile();
+  const uniqueNewFeeds = getNewFeedItemsOnly(previousFeeds, combinedFeeds);
+
+  console.log(JSON.stringify(uniqueNewFeeds));
 })();
 
 async function readPreviousFeedsFromFile(): Promise<CombinedFeeds> {
@@ -17,9 +21,58 @@ async function readPreviousFeedsFromFile(): Promise<CombinedFeeds> {
   return oldFeed as CombinedFeeds;
 }
 
-// function compareFeeds(
-//   oldFeed: CombinedFeeds,
-//   newFeed: CombinedFeeds
-// ): CombinedFeeds {
+function getNewFeedItemsOnly(
+  oldCombinedFeeds: CombinedFeeds,
+  newCombinedFeeds: CombinedFeeds
+): CombinedFeeds {
+  /*
+  - pointer to newest item of both feeds
+  - if pubDate of newFeed item is more recent than item in oldFeed, add it to the output
+  - if pubDate is the same or older stop
+  */
 
-// }
+  const resultFeeds: ProcessedFeed[] = [];
+
+  const oldFeeds = oldCombinedFeeds.feeds;
+  const newFeeds = newCombinedFeeds.feeds;
+
+  //map old feeds for fast lookup and comparison
+  const oldFeedMap = new Map<string, ProcessedFeed>();
+  for (const oldFeed of oldFeeds) {
+    oldFeedMap.set(oldFeed.title, oldFeed);
+  }
+
+  //map through new feeds and only return ones  with new items
+  for (const singleNewFeed of newFeeds) {
+    // brand new feed - no results for the old one so push the entire feed
+    if (!oldFeedMap.has(singleNewFeed.title)) resultFeeds.push(singleNewFeed);
+    else {
+      const singleOldFeed = oldFeedMap.get(singleNewFeed.title);
+      assert.ok(singleOldFeed !== undefined, "Old Feed is undefined");
+
+      // old feed had no items and new feed has items, so just use the entire new feed
+      if (
+        singleOldFeed.items.length === 0 &&
+        singleNewFeed.items.length !== 0
+      ) {
+        resultFeeds.push(singleNewFeed);
+        continue;
+      }
+
+      //get all new items from this feed
+      const mostRecentOldItem = singleOldFeed.mostRecentPubDate;
+      const singleFeedResultItems = singleNewFeed.items.filter(
+        (item) => item.pubDate > mostRecentOldItem
+      );
+
+      if (singleFeedResultItems.length === 0) continue;
+
+      resultFeeds.push({ ...singleNewFeed, items: singleFeedResultItems });
+    }
+  }
+
+  return {
+    feeds: resultFeeds,
+    timeUpdated: new Date().toString(),
+  };
+}
